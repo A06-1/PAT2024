8a705d53a008a8ec23b2efebc31c11f16551eea4
A06-1 <126359759+A06-1@users.noreply.github.com>
2023-10-24 11:14:27 +0200

    Update Backend.java

--- Backend.java
+++ Backend.java
@@ -21,9 +21,9 @@ import java.util.List;
 
         // Writing to a text file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write("Hello, this is a test line.");
+            writer.write("Successful line.");
             writer.newLine(); // Add a newline
-            writer.write("This is another line.");
+                writer.write("Successful next line.");
         } catch (IOException e) {
             System.err.println("Error writing to the file: " + e.getMessage());
         }
